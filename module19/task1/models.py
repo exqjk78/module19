fbfd7708fa93c9baf90097339b87a98e5ffa51ad
exq <purov.bordan@gmail.com>
2025-01-26 15:08:39 +0300

    task3

--- module19/task1/models.py
+++ module19/task1/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Buyer(models.Model):
-    name = models.CharField(max_length=30)
-    balance = models.DecimalField(decimal_places=2,max_digits=9,default=0)
-    age = models.IntegerField()
+    name = models.CharField(max_length=30,verbose_name='Имя покупателя')
+    balance = models.DecimalField(decimal_places=2,max_digits=9,default=0,verbose_name='Баланс')
+    age = models.IntegerField(verbose_name='Возраст')
 
     def __str__(self):
         return self.name
 
 class Game(models.Model):
-    title = models.CharField(max_length=100,default='Unnamed')
-    cost = models.DecimalField(decimal_places=2,max_digits=9)
-    size = models.DecimalField(decimal_places=2,max_digits=9)
+    title = models.CharField(max_length=100,default='Unnamed',verbose_name='Название игры')
+    cost = models.DecimalField(decimal_places=2,max_digits=9,verbose_name='Стоимость')
+    size = models.DecimalField(decimal_places=2,max_digits=9,verbose_name='Размер (мегабайты)')
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer,related_name='buyers')
